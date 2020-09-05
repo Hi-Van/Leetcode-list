a3d245ce1622d73515abbd82c771c17e6d75a0c0
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-09-04 21:34:37 -0400

    add solutions

---
+++ 217.js
@@ -0,0 +1,13 @@
+var containsDuplicate = function (nums) {
+    let result = false;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] == nums[j]) {
+                result = true;
+            }
+        }
+    }
+
+    return result
+};
